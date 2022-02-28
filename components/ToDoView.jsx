babcd8da8f5c981debf47fd426ecb03163fcc8aa
Cody Clark <opuseleven@protonmail.com>
2022-02-27 19:29:01 -0600

    Updated ToDoView to use CompletedButton component.

--- components/ToDoView.jsx
+++ components/ToDoView.jsx
@@ -1,7 +1,10 @@
+import { CompletedButton } from '../components';
+
 function ToDoView({ todo }) {
 
   return (
     <div key={todo.id}>
+      <CompletedButton completed={todo.completed} />
       <h4>{todo.name}</h4>
       <p>{todo.completed}</p>
     </div>
