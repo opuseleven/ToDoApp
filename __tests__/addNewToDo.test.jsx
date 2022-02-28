f7228bfa7d9a08b60dc02f394076d8a355db2156
Cody Clark <opuseleven@protonmail.com>
2022-02-28 11:39:20 -0600

    Added tests for getNewToDo service

---
+++ __tests__/addNewToDo.test.jsx
@@ -0,0 +1,20 @@
+import { addNewToDo, getToDo } from '../services';
+
+describe('addNewToDo function', () => {
+
+  const td1 = getToDo(1, 'test1', false);
+  const td2 = getToDo(2, 'test2', false);
+  const td3 = getToDo(3, 'test3', false);
+  const td4 = getToDo(4, 'test4', false);
+
+  let arr = [td1, td2, td3, td4];
+  function setArr(newArr) {
+    arr = newArr
+  }
+
+  it('Adds a ToDo object to arr', () => {
+    const newToDo = getToDo(5, 'test5', false);
+    addNewToDo(newToDo, arr, setArr);
+    expect(arr.length).toBe(5);
+  })
+})
