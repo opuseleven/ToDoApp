7605d10129da19596e95955a350d48a60db78246
Cody Clark <opuseleven@protonmail.com>
2022-03-01 10:18:43 -0600

    Added functionality to AddToDoButton component

--- components/AddToDoButton.jsx
+++ components/AddToDoButton.jsx
@@ -1,8 +1,16 @@
-function AddToDoButton() {
+import { addNewToDo, getToDo } from '../services';
+
+function AddToDoButton({ arr, setArr }) {
+
+  function handleClick() {
+    const newId = arr.length + 1;
+    const newToDo = getToDo(newId, '', false);
+    addNewToDo(newToDo, arr, setArr);
+  }
 
   return (
     <div>
-      <button>+</button>
+      <button onClick={handleClick}>+</button>
     </div>
   )
 }
