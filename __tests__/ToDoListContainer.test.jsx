68b5827a866d6998b4ffd6174271e3cc3646d81a
Cody Clark <opuseleven@protonmail.com>
2022-03-31 22:57:23 -0500

    Added tests for ToDoListContainer component

--- __tests__/ToDoListContainer.test.jsx
+++ __tests__/ToDoListContainer.test.jsx
@@ -1,50 +1,59 @@
 import { ToDoListContainer } from '../components';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 describe('ToDoListContainer component', () => {
 
-  const testtodo1 = {
+  let testtodo1 = {
     id: 1,
     name: 'do something',
     completed: false
   }
-  const testtodo2 = {
+  let testtodo2 = {
     id: 2,
     name: 'do something else',
     completed: false,
   }
-  const testtodo3 = {
+  let testtodo3 = {
     id: 3,
     name: 'done something',
     completed: true
   }
-  const testtodo4 = {
+  let testtodo4 = {
     id: 4,
     name: 'another one',
     completed: false
   }
-  const testtodo5 = {
+  let testtodo5 = {
     id: 5,
     name: 'done something else',
     completed: true
   }
 
-  const arr = [testtodo1, testtodo2, testtodo3, testtodo4, testtodo5];
+  let arr = [testtodo1, testtodo2, testtodo3, testtodo4, testtodo5];
 
   it('Component renders', () => {
     render(<ToDoListContainer list={arr} />);
-    const headings = screen.getAllByRole('heading');
+    const headings = screen.getAllByRole('textbox');
     expect(headings[0]).toBeDefined();
   })
 
   it('Sorts lists and passes data', () => {
     render(<ToDoListContainer list={arr} />);
-    const headings = screen.getAllByRole('heading');
-    expect(headings[0]).toHaveTextContent('do something');
-    expect(headings[1]).toHaveTextContent('do something else');
-    expect(headings[2]).toHaveTextContent('another one');
-    expect(headings[3]).toHaveTextContent('done something');
-    expect(headings[4]).toHaveTextContent('done something else');
+    const headings = screen.getAllByRole('textbox');
+    expect(headings[0].value).toBe('do something');
+    expect(headings[1].value).toBe('do something else');
+    expect(headings[2].value).toBe('another one');
+    expect(headings[3].value).toBe('done something');
+    expect(headings[4].value).toBe('done something else');
+  })
+
+  it('Updates data', () => {
+    render(<ToDoListContainer list={arr} />);
+    const headings = screen.getAllByRole('textbox');
+    const checkboxes = screen.getAllByRole('checkbox');
+    fireEvent.click(checkboxes[0]);
+    expect(testtodo1.completed).toBe(true);
+    expect(headings[0].value).toBe('do something');
+    expect(checkboxes[0].checked).toBe(true);
   })
 })
