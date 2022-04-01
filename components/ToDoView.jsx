379061e5f18aedb1953819d6435304292be2318a
Cody Clark <opuseleven@protonmail.com>
2022-03-31 21:15:20 -0500

    Fixed ToDoView component

--- components/ToDoView.jsx
+++ components/ToDoView.jsx
@@ -1,11 +1,19 @@
 import { CompletedButton } from '../components';
+import { useState, useEffect } from 'react';
+import { updateToDo } from '../services';
 
 function ToDoView({ todo }) {
 
+  const [name, setName] = useState(todo.name);
+
+  useEffect(() => {
+    updateToDo(todo, name);
+  }, [name])
+
   return (
     <div key={todo.id}>
-      <CompletedButton completed={todo.completed} />
-      <h4>{todo.name}</h4>
+      <CompletedButton todo={todo} />
+      <input type="text" value={name} onChange={() => setName(event.target.value)} />
       <p>{todo.completed}</p>
     </div>
   )
