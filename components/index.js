43271159d1a89ace6548af033792455d90286765
Cody Clark <opuseleven@protonmail.com>
2022-03-31 22:05:33 -0500

    Removed unnecessary CompletedList component

--- components/index.js
+++ components/index.js
@@ -1,6 +1,5 @@
 export * from './ToDoListContainer';
 export * from './ToDoList';
-export * from './CompletedList';
 export * from './ToDoView';
 export * from './AddToDoButton';
 export * from './CompletedButton';
