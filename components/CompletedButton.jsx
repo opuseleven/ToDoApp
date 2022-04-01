bb064ea6ca21d58518737e4ab81efde9af7cf2f2
Cody Clark <opuseleven@protonmail.com>
2022-03-31 21:16:03 -0500

    Fixed CompletedButton component to update ToDo object completed prop

--- components/CompletedButton.jsx
+++ components/CompletedButton.jsx
@@ -1,12 +1,18 @@
-function CompletedButton({ completed }) {
+import { updateCompleted } from '../services';
 
-  function handleChange() {
-    
-  }
+import { useState, useEffect } from 'react';
+
+function CompletedButton({ todo }) {
+
+  const [checked, setChecked] = useState(todo.completed);
+
+  useEffect(() => {
+    updateCompleted(todo, checked);
+  }, [checked])
 
   return (
     <div>
-      <input type="checkbox" id="completedbutton" checked={completed} onChange={handleChange} />
+      <input type="checkbox" id="completedbutton" checked={checked} onChange={() => setChecked(!checked)} />
     </div>
   )
 }
