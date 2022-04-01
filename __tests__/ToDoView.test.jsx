c37c92a5150e40e823f694c039b7c04314659054
Cody Clark <opuseleven@protonmail.com>
2022-03-31 21:17:55 -0500

    Added additional tests

--- __tests__/ToDoView.test.jsx
+++ __tests__/ToDoView.test.jsx
@@ -1,10 +1,10 @@
 import { ToDoView } from '../components';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { getToDo } from '../services';
 
 describe('ToDoView component', () => {
 
-  const testToDo = {
+  let testToDo = {
     id: 1,
     name: 'test todo',
     completed: false
@@ -12,6 +12,25 @@ describe('ToDoView component', () => {
 
   it('Component Renders', () => {
     render(<ToDoView  todo={testToDo} />);
-    expect(screen.getByRole('heading')).toHaveTextContent('test todo');
+    expect(screen.getByRole('textbox')).toBeDefined();
+  })
+
+  it('Passes Name value', () => {
+    render(<ToDoView todo={testToDo} />);
+    expect(screen.getByRole('textbox').value).toBe('test todo');
+  })
+
+  it('Updates values onChange', () => {
+    render(<ToDoView todo={testToDo} />);
+    let textbox = screen.getByRole('textbox');
+    fireEvent.change(textbox, {target: {value: 'test change'}});
+    expect(testToDo.name).toBe('test change');
+  })
+
+  it('Completed button updates todo.completed', () => {
+    render(<ToDoView todo={testToDo} />);
+    let checkbox = screen.getByRole('checkbox');
+    fireEvent.click(checkbox);
+    expect(testToDo.completed).toBe(true);
   })
 })
