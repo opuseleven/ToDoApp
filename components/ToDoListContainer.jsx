f92dc5395683692936df266e6502c47c6668f994
Cody Clark <opuseleven@protonmail.com>
2022-03-31 22:56:21 -0500

    Fixed ToDoListContainer component to update state of sorted lists

--- components/ToDoListContainer.jsx
+++ components/ToDoListContainer.jsx
@@ -1,15 +1,20 @@
 import styles from '../styles/Components.module.css';
 import { ToDoList, CompletedList } from '../components';
 import { getSortedLists } from '../services';
+import { useState, useEffect } from 'react';
 
 function ToDoListContainer({ list }) {
 
-  const sortedLists = getSortedLists(list);
+  const [sortedLists, setSortedLists] = useState(getSortedLists(list));
+
+  useEffect(() => {
+    setSortedLists(getSortedLists(list));
+  }, [list])
 
   return (
     <div>
       <ToDoList todolist={sortedLists.todoArr} />
-      <CompletedList completedlist={sortedLists.completedArr} />
+      <ToDoList todolist={sortedLists.completedArr} />
     </div>
   )
 }
