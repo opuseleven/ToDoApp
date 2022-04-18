10e9f85c11d68d57c4a26670a948d29fd035a9ef
Cody Clark <opuseleven@protonmail.com>
2022-04-18 13:35:03 -0500

    Added tests for DeleteButton component

---
+++ __tests__/DeleteButton.test.jsx
@@ -0,0 +1,25 @@
+import { DeleteButton } from '../components';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { getToDo } from '../services';
+
+describe('DeleteButton Component', () => {
+
+  const td1 = getToDo(1, 'test1', false);
+  const td2 = getToDo(2, 'test2', false);
+
+  let arr = [td1, td2];
+
+  it('Component renders', () => {
+    render(<DeleteButton />);
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent('x');
+  })
+
+  it('onClick runs removeToDo service', () => {
+    render(<DeleteButton todo={td2} list={arr} />)
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(arr).toEqual([td1]);
+  })
+})
