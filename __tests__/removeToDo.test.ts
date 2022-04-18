55bf5779cd2db3e08cb4363168049e20208ad129
Cody Clark <opuseleven@protonmail.com>
2022-04-18 10:16:31 -0500

    Added tests for removeToDo service

---
+++ __tests__/removeToDo.test.ts
@@ -0,0 +1,24 @@
+import { removeToDo, getToDo } from '../services';
+
+describe('removeToDo service', () => {
+
+  const td1 = getToDo(1, 'test1', false);
+  const td2 = getToDo(2, 'test2', false);
+  const td3 = getToDo(3, 'test3', false);
+  const td4 = getToDo(4, 'test4', false);
+  const td5 = getToDo(5, 'test5', false);
+
+  let arr = [td1, td2, td3, td4, td5];
+
+  it('Removes a ToDo object from the arr', () => {
+    removeToDo(5, arr);
+    expect(arr.length).toBe(4);
+    expect(arr).toEqual([td1, td2, td3, td4]);
+  })
+
+  it('Removes ToDo object from middle of array', () => {
+    removeToDo(2, arr);
+    expect(arr.length).toBe(3);
+    expect(arr).toEqual([td1, td3, td4]);
+  })
+})
