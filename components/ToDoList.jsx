5464df171fa2927ab14dc705aa35ef9bcd030135
Cody Clark <opuseleven@protonmail.com>
2022-04-18 10:24:51 -0500

    Added useEffect to update displayed lists

--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -1,14 +1,22 @@
 import styles from '../styles/Components.module.css';
 import { ToDoView } from '../components';
+import { useState, useEffect } from 'react';
 
-function ToDoList({ todolist }) {
+
+function ToDoList({ todolist, list }) {
+
+  const [displayedList, setDisplayedList] = useState(todolist);
+
+  useEffect(() => {
+    setDisplayedList(todolist);
+  }, [list])
 
   return (
     <div>
       {
         todolist.map(t => (
           <div key={t.id} className={styles.todolist}>
-            <ToDoView todo={t} />
+            <ToDoView todo={t} list={list} />
           </div>
         ))
       }
