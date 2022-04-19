2e21bda2ade6c028c0cbf87ab53adc698413cbc4
Cody Clark <opuseleven@protonmail.com>
2022-04-18 20:45:06 -0500

    Fixed

--- services/removeToDo.ts
+++ services/removeToDo.ts
@@ -1,10 +1,14 @@
 import { ToDo } from '../types';
 
-function removeToDo(id: number, list: ToDo[]) {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].id == id) {
-      list.splice(i, 1);
+function removeToDo(id: number, arr: ToDo[], setArr: React.Dispatch<React.SetStateAction<ToDo[]>>) {
+
+  let newArr = arr;
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i].id == id) {
+      newArr.splice(i, 1);
     }
   }
+  setArr(newArr);
 }
 export { removeToDo };
