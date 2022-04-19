693565fbc29ad46a4dcfd1d8f54c0854933d6849
Cody Clark <opuseleven@protonmail.com>
2022-04-19 14:42:46 -0500

    Call sort() each time completed variable is updated

--- components/CompletedButton.jsx
+++ components/CompletedButton.jsx
@@ -2,12 +2,13 @@ import { updateCompleted } from '../services';
 import styles from '../styles/Components.module.css';
 import { useState, useEffect } from 'react';
 
-function CompletedButton({ todo }) {
+function CompletedButton({ todo, sort }) {
 
   const [checked, setChecked] = useState(todo.completed);
 
   useEffect(() => {
     updateCompleted(todo, checked);
+    sort();
   }, [checked])
 
   return (
