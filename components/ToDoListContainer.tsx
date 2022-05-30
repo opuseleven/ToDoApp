c585a7ec1fc3c518aa6c67d53a775388ebd2f6fd
Cody Clark <opuseleven@protonmail.com>
2022-05-30 11:29:00 -0500

    Changed ToDoListContainer component to typescript.

--- components/ToDoListContainer.tsx
+++ components/ToDoListContainer.tsx
@@ -1,9 +1,15 @@
 import styles from '../styles/Components.module.css';
-import { ToDoList, CompletedList } from '../components';
+import { ToDoList } from '../components';
 import { getSortedLists } from '../services';
 import { useState, useEffect } from 'react';
+import { ToDo } from '../types';
 
-function ToDoListContainer({ arr, setArr }) {
+interface ToDoListContainerProps {
+  arr: ToDo[],
+  setArr: React.Dispatch<React.SetStateAction<ToDo[]>>
+}
+
+const ToDoListContainer: React.FC<ToDoListContainerProps> = ({ arr, setArr }) => {
 
   const [sortedLists, setSortedLists] = useState(getSortedLists(arr));
 
