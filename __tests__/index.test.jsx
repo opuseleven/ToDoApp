7f66ba93e077497deaeaf49071a81074df0f1adf
Cody Clark <opuseleven@protonmail.com>
2022-07-01 14:46:15 -0500

    Added test to sort list

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -62,4 +62,37 @@ describe('Home page', () => {
     headings = screen.getAllByRole('textbox');
     expect(headings.length).toBe(2);
   })
+
+  it('Clicking checkbox sorts list by completed', async () => {
+    render(<Home />);
+    let headings = screen.getAllByRole('textbox');
+    let buttons = screen.getAllByRole('button');
+    act(() => {
+      fireEvent.change(headings[0], {target: {value: 'one'}});
+      fireEvent.click(buttons[0]);
+    })
+    headings = screen.getAllByRole('textbox');
+    act(() => {
+      fireEvent.change(headings[1], {target: {value: 'two'}});
+      fireEvent.click(buttons[0]);
+    })
+    headings = screen.getAllByRole('textbox');
+    act(() => {
+      fireEvent.change(headings[2], {target: {value: 'three'}});
+    })
+    expect(headings[0].value).toBe('one');
+    expect(headings[1].value).toBe('two');
+    expect(headings[2].value).toBe('three');
+    expect(headings.length).toBe(3);
+    let checkboxes = screen.getAllByRole('checkbox');
+    await act(async () => {
+      fireEvent.click(checkboxes[1]);
+      await new Promise(r => setTimeout(r, 1000));
+    })
+    headings = screen.getAllByRole('textbox');
+    expect(headings.length).toBe(3);
+    expect(headings[0].value).toBe('one');
+    expect(headings[1].value).toBe('three');
+    expect(headings[2].value).toBe('two');
+  })
 })
