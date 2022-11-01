ae256ede0166bc7d570adf2538783090f493da67
Cody Clark <opuseleven@protonmail.com>
2022-11-01 15:24:37 -0500

    Added additional tests for AddToDoButton component

--- __tests__/AddToDoButton.test.jsx
+++ __tests__/AddToDoButton.test.jsx
@@ -1,11 +1,34 @@
 import { AddToDoButton } from '../components';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('AddToDoButton component', () => {
 
+  let arr = [];
+  function setArr(newArr) {
+    arr = newArr;
+  }
+
   it('Component renders', () => {
     render(<AddToDoButton />);
     expect(screen.getByRole('button')).toHaveTextContent('+');
+  })
+
+  it('Component functions to add a new empty ToDo obj to arr', () => {
+    render(<AddToDoButton arr={arr} setArr={setArr} />);
+    act(() => {
+      fireEvent.click(screen.getByText('+'));
+    })
+    expect(arr.length).toBe(1);
+    expect(arr[0].id).toBe(1);
+  })
+
+  it('Functions to add another empty ToDo obj to arr', () => {
+    render(<AddToDoButton arr={arr} setArr={setArr} />);
+    act(() => {
+      fireEvent.click(screen.getByText('+'));
+    })
+    expect(arr.length).toBe(2);
+    expect(arr[1].id).toBe(2);
   })
 })
