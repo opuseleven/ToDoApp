00d8d03e6a604fbc0444e2fd7585cae10c31abc1
Cody Clark <opuseleven@protonmail.com>
2022-11-01 13:57:58 -0500

    Fixed imports

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,16 +1,16 @@
 import styles from '../styles/Components.module.css';
 import { ToDoView } from '../components';
-import { useState, useEffect } from 'react';
+import { FC, Dispatch, SetStateAction, useState, useEffect } from 'react';
 import { ToDo } from '../types';
 
 interface ToDoListProps {
   todolist: ToDo[],
   arr: ToDo[],
-  setArr: React.Dispatch<React.SetStateAction<ToDo[]>>,
+  setArr: Dispatch<SetStateAction<ToDo[]>>,
   sort: VoidFunction
 }
 
-const ToDoList: React.FC<ToDoListProps> = ({ todolist, arr, setArr, sort }) => {
+const ToDoList: FC<ToDoListProps> = ({ todolist, arr, setArr, sort }) => {
 
   const [displayedList, setDisplayedList] = useState(todolist);
 
